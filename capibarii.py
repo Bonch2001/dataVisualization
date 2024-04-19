4579cc58ad90ef15bb0370ab852c4f969479c062
alin <bonciu_alin@yahoo.com>
2024-04-20 00:29:47 +0300

    csv

--- capibarii.py
+++ capibarii.py
@@ -1,1 +1,7 @@
 
+import pandas
+
+data = pandas.read_parquet("data/part-00000-ac5c3a2a-c89d-4817-8a00-1ba717b8f279-c000.snappy.parquet", engine = "pyarrow")
+# print(data.dtypes)
+
+data.to_csv('company_data.csv', index=False)
