9bf20ac877163b4176ff8d45ed69232ac2dddf77
alin <bonciu_alin@yahoo.com>
2024-04-20 15:53:59 +0300

    clustering

--- capibarii.py
+++ capibarii.py
@@ -6,6 +6,10 @@ from sklearn.cluster import KMeans
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import hdbscan
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
 
 class DataRecord:
     def __init__(self, **kwargs):
@@ -16,8 +20,10 @@ df = pandas.read_parquet("data/part-00000-ac5c3a2a-c89d-4817-8a00-1ba717b8f279-c
 
 # print(data)
 
+# np.savetxt(r'np.txt', df.values, fmt='%s', delimiter=' ', encoding='utf-8')
+
 records = [DataRecord(**row.to_dict()) for index, row in df.iterrows()]
-# records e o lista de obiecte; fiecare obiect e un rand din DataFrame, avand ca atribute coloanele
+""" # records e o lista de obiecte; fiecare obiect e un rand din DataFrame, avand ca atribute coloanele
 
 # print(records[11].legal_names[0])
 
@@ -51,7 +57,7 @@ df['processed_keywords'] = df['keywords'].apply(preprocess_text)
 vectorizer = CountVectorizer(min_df=1, max_features=1000)
 word_matrix = vectorizer.fit_transform(df['processed_keywords'])
 
-print("Shape of matrix:", word_matrix.shape) # (36000, 1000)
+# print("Shape of matrix:", word_matrix.shape) # (36000, 1000)
 
 k = 10  # Set the number of clusters
 kmeans = KMeans(n_clusters=k, random_state=42)
@@ -63,3 +69,20 @@ df['cluster'] = clusters
 for i in range(k):
     print(f"Cluster {i}:")
     print(df[df['cluster'] == i]['keywords'].head(10), "\n")
+ """
+
+df['keywords'] = df['keywords'].astype(str)
+# Convert keywords into TF-IDF vectors
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(df['keywords'])
+
+# Optional: Standardize features by removing the mean and scaling to unit variance
+X = StandardScaler(with_mean=False).fit_transform(X)
+
+# HDBSCAN clustering
+hdbscan_cluster = hdbscan.HDBSCAN(min_cluster_size=2, metric='euclidean', cluster_selection_method='eom')
+clusters = hdbscan_cluster.fit_predict(X)
+
+# Add cluster labels to the DataFrame
+df['cluster'] = clusters
+print(df)
